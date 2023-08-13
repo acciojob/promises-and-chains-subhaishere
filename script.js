935bcebc19616f0a55546b69815121a90b592f28
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 00:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function check(){
+	let age = document.getElementById("age").value;
+	let name = document.getElementById("name").value;
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+			alert(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				alert(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000)
+	})
+}
